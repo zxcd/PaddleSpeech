7dc806dc1d957ed7f316530c9ae0db626e19967d
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2024-11-25 11:27:48 +0800

    run with aishell/asr3 (#3904)

--- paddlespeech/s2t/models/wav2vec2/modules/wav2vec2_model.py
+++ paddlespeech/s2t/models/wav2vec2/modules/wav2vec2_model.py
@@ -714,13 +714,13 @@ class MultiheadAttention(nn.Layer):
             else:
                 if self.beam_size > 1 and bsz == key.size(1):
                     # key is [T, bsz*beam_size, C], reduce to [T, bsz, C]
-                    key = key.view(
-                        key.size(0), -1, self.beam_size,
-                        key.size(2))[:, :, 0, :]
+                    key = key.reshape(
+                        [key.size(0), -1, self.beam_size,
+                         key.size(2)])[:, :, 0, :]
                     if key_padding_mask is not None:
-                        key_padding_mask = key_padding_mask.view(
-                            -1, self.beam_size,
-                            key_padding_mask.size(1))[:, 0, :]
+                        key_padding_mask = key_padding_mask.reshape(
+                            [-1, self.beam_size,
+                             key_padding_mask.size(1)])[:, 0, :]
                 k = self.k_proj(key)
                 v = self.v_proj(key)
 
